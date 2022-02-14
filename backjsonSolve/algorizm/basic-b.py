86d030cff1a547a4740cf5c84109a2874b3b94d4
jinu12 <kzkju@naver.com>
2022-02-14 23:59:41 +0900

    update basic-b

--- backjsonSolve/algorizm/basic-b.py
+++ backjsonSolve/algorizm/basic-b.py
@@ -22,14 +22,41 @@
 # else:
 #     print("F")
 # 사분면 고르기
+# from sys import stdin
+# x = int(stdin.readline())
+# y = int(stdin.readline())
+# if x > 0 and y > 0:
+#     print(1)
+# elif x < 0 and y > 0:
+#     print(2)
+# elif x < 0 and y < 0:
+#     print(3)
+# else:
+#     print(4)
+# 45분 일찍
+# from sys import stdin
+# H, M = stdin.readline().split()
+# H, M = int(H), int(M)
+# if M < 45:
+#     if H == 0:
+#         H = 23
+#         M = (M + 60) - 45
+#     else:
+#         H = H - 1
+#         M = (M + 60) - 45
+# else:
+#     M = M - 45
+# print("{} {}".format(H, M))
+# 오븐 시계
 from sys import stdin
-x = int(stdin.readline())
-y = int(stdin.readline())
-if x > 0 and y > 0:
-    print(1)
-elif x < 0 and y > 0:
-    print(2)
-elif x < 0 and y < 0:
-    print(3)
-else:
-    print(4)
+H, M = stdin.readline().split()
+H, M = int(H), int(M)
+C = int(stdin.readline())
+if M + C >= 60:
+    H = H + (M+C) // 60
+    M = (M+C) - 60
+    if H >= 24:
+        H = H - 24
+    else:
+        M = M + C
+print("{} {}".format(H, M))
