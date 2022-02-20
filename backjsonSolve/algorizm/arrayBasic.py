15c7dda2f1d4c48b6101d8d70edcda9e52620589
jinu12 <kzkju@naver.com>
2022-02-20 21:51:07 +0900

    add algorizm arrayBasic.py

---
+++ backjsonSolve/algorizm/arrayBasic.py
@@ -0,0 +1,9 @@
+# 수 정렬하기
+
+from sys import stdin
+N = int(stdin.readline())
+X = sorted(list(int(stdin.readline()) for i in range(N)))
+for i in X:
+    print(i)
+
+#
--- backjsonSolve/algorizm/basic4.py
+++ backjsonSolve/algorizm/basic4.py
@@ -14,35 +14,26 @@
 # print(factorial(N))
 
 # 파보나치 수열
-from sys import stdin
-N = int(stdin.readline())
-fabo = list()
+# from sys import stdin
+# N = int(stdin.readline())
+# fabo = list()
+#
+#
+# def fabonachi(num):
+#     global fabo
+#     for i in range(0, num+1):
+#         if i == 0:
+#             fabo.append(0)
+#         elif i <= 2:
+#             fabo.append(1)
+#         else:
+#             fabo.append(fabo[i-1] + fabo[i-2])
+#     return fabo[num]
+#
+#
+# print(fabonachi(N))
 
 
-def fabonachi(num):
-    global fabo
-    for i in range(0, num+1):
-        if i == 0:
-            fabo.append(0)
-        elif i <= 2:
-            fabo.append(1)
-        else:
-            fabo.append(fabo[i-1] + fabo[i-2])
-    return fabo[num]
 
 
-print(fabonachi(N))
 
-
-# test = list(int(i) for i in range(3))
-# print(test)
-# test.append(test[2] + test[1])
-# print(test)
-# for i in range(10):
-#     if i == 0:
-#         test.append(1)
-#     if i == 1:
-#         test.append(2)
-#     else:
-#         sum(test[i-1],test[i-2]
-# print(test)
