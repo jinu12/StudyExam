068b6e1e71bc4342bef0c543297fc412bcdfec0a
jinu12 <kzkju@naver.com>
2022-02-21 15:07:13 +0900

    add mathbasic2 commit

---
+++ backjsonSolve/algorizm/mathbasic2.py
@@ -0,0 +1,17 @@
+# 소수 찾기 1
+# from sys import stdin
+# cnt1 = 0
+# cnt2 = 0
+# N = int(stdin.readline())
+# A = stdin.readline().split()
+# X = list(int(A[i]) for i in range(N))
+# for i in range(len(X)):
+#     for j in range(1, X[i] + 1):
+#         if X[i] % j == 0:
+#             cnt1 += 1
+#     if cnt1 == 2:
+#         cnt2 += 1
+#     cnt1 = 0
+# print(cnt2)
+
+
